5e6fba3e17ed2cc57b7797823f7056b7c409a368
Alexandre Peyrichout <a.peyrichout@gmail.com>
2019-09-27 13:43:49 +0200

    hahahaha

--- scripts.js
+++ scripts.js
@@ -21,23 +21,29 @@ buttonSend.addEventListener('click', modalFormFunction);
 
 
 
-///// TOP/BOTTOM NAVBAR AFTER SCROLL 100VH /////////////
+///// TOP/BOTTOM NAVBAR AFTER SCROLLING 100VH /////////////
 
 /* DOM shortcuts */
 const navBar = document.getElementById('nav');
 const carousel = document.getElementById('carousel');
 const foot = document.getElementById('foot');
+const logoDefault = document.getElementById('logo');
+const logoUp = document.getElementById('logo-top');
 
 /* Function */
 const toggleNavbar = () => {
-    if (window.scrollY > carousel.offsetHeight){
+    if (window.scrollY >= carousel.offsetHeight){
         navBar.style.marginTop = `${carousel.offsetHeight - 40}px`;
         foot.style.marginBottom = '40px';
         navBar.style.transitionDuration = '1s';
         navBar.style.boxShadow = '0 -0.3em 0.5em black';
+        logoDefault.style.display = 'none';
+        logoUp.style.display = 'flex';
     } else{
         navBar.style.marginTop = '';
         navBar.style.boxShadow = '0 0.3em 0.5em teal';
+        logoDefault.style.display = 'flex';
+        logoUp.style.display = 'none';
     }
 }
 
@@ -48,7 +54,6 @@ document.addEventListener('scroll', toggleNavbar);
 
 
 ///// APPARITION DU TITRE ///////////////////////////////
-
 /* First loading */
 document.getElementById("fade").style.opacity = '1';
 /////////////////////////////////////////////////////////
